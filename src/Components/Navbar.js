92b273fe6fd59843aa7348278765de5dbbac3e77
Jason Kluge <jason.kluge5813@gmail.com>
2021-11-29 10:14:16 -0600

    Adding component and pages file

---
+++ src/Components/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Button } from 'reactstrap';
+import'/Users/jakluge/Desktop/my-app/src/App.css';
+
+export default class Navbar extends React.Component {
+  render() {
+    return (
+      <div>
+        <Button color="primary">primary</Button>{' '}
+        <Button color="secondary">secondary</Button>{' '}
+        <Button color="success">success</Button>{' '}
+        <Button color="info">info</Button>{' '}
+        <Button color="warning">warning</Button>{' '}
+        <Button color="danger">danger</Button>{' '}
+        <Button color="link">link</Button>
+      </div>
+    );
+  }
+}
+
