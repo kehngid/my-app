4613f17d3a49582d7c8091c5392f1973d7d2c024
Jason Kluge <jason.kluge5813@gmail.com>
2022-01-02 17:29:58 -0600

    Add css style sheet

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import { Button } from 'reactstrap';
-import'/Users/jakluge/Desktop/my-app/src/App.css';
+import'../Stylesheets/Navbar.css';
 
 export default class Navbar extends React.Component {
   render() {
     return (
       <div>
-        <Button color="primary">primary</Button>{' '}
-        <Button color="secondary">secondary</Button>{' '}
-        <Button color="success">success</Button>{' '}
-        <Button color="info">info</Button>{' '}
-        <Button color="warning">warning</Button>{' '}
-        <Button color="danger">danger</Button>{' '}
-        <Button color="link">link</Button>
+        <Button className="button">Home</Button>{' '}
+        <Button className="button">Projects</Button>{' '}
+        <Button className="button">About</Button>
       </div>
     );
   }
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,6 +1,6 @@
 import { render } from '@testing-library/react';
 import React from 'react';
-import Title from '../Stylesheets/Home.css';
+import '../Stylesheets/Home.css';
 
 function Home()
 {
