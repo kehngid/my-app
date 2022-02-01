e6a0c3f14232389c86323f821ad5f8a8a1e6feb2
Jason Kluge <jason.kluge5813@gmail.com>
2022-02-01 13:53:05 -0600

    Got navbar apperance working, now making links appear 'active' onClick problem

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import {  Nav, NavItem, NavLink } from 'reactstrap';
-import { Link } from "react-router-dom";
 import'../Stylesheets/Navbar.css';
-import { propTypes } from 'react-bootstrap/esm/Image';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-export default class Navbar extends React.Component {
-
+export default class Example extends React.Component {
   render() {
     return (
       <div>
-        <Nav>
+        <Nav pills>
           <NavItem>
-            <NavLink href="/" active>Home</NavLink>
+            <NavLink href="/home">Home</NavLink>
           </NavItem>
           <NavItem>
             <NavLink href="/projects">Projects</NavLink>
