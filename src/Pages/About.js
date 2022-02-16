4722e9f6dd78b45ba1b2cb3d01f228a8070a0574
Jason Kluge <jason.kluge5813@gmail.com>
2022-02-15 18:05:50 -0600

    Added links and more responsive display

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ProfilePic from '../Components/ProfilePic.js';
 import '../Stylesheets/About.css'
+import '../Stylesheets/Page.css'
 
 function About()
 {
     return (
-      <div>
+      <body>
         <div className="column1">
           <ProfilePic/>
         </div>
@@ -13,19 +14,24 @@ function About()
           <div className="name">
             <h1>Jason Kluge</h1>
           </div>
+          <br></br>
           <div className="links">
-            <h2>jason.kluge5813@gmail.com</h2>
-            <h2>Github link</h2>
-            <h2>LinkedIn link</h2>
+            <a href="mailto: jason.kluge5813@gmail.com">jason.kluge5813@gmail.com</a>
+            <br></br>
+            <a href="https://github.com/kehngid">Github</a>
+            <br></br>
+            <a href="https://www.linkedin.com/in/jakluge/">LinkedIn</a>
           </div>
+          <br></br>
           <div className="title">
             <h1 >About</h1>
           </div>
           <div className="paragraph">
           <p>I'm a computer science major and statistics minor at the University of Texas at Dallas. I created this website to showcase projects I've worked on and am currently working on. </p>
+          <p>My hobbies include learning and keeping insects, reading books, fencing, and playing a popular TCG called Magic: the Gathering.</p>
           </div>
         </div>
-      </div>
+      </body>
     );
 }
 
